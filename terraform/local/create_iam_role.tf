a6e891bc72c6e2be37dff6395ca59e8796f4fb03
bhnd38 <ebonearl@naver.com>
2024-09-23 17:44:17 +0900

    edited create_iam_role.tf

--- terraform/local/create_iam_role.tf
+++ terraform/local/create_iam_role.tf
@@ -254,4 +254,3 @@ resource "aws_iam_role_policy_attachment" "alb_ingress_controller_attach" {
   policy_arn = aws_iam_policy.alb_ingress_controller_policy.arn
   role       = aws_iam_role.alb_controller_role.name
 }
-
